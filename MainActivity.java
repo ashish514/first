f0c1ada56521b479a5b1165d7dbf8c46e687e41f
ashish514 <ashishkr.444@gmail.com>
2016-05-14 08:09:56 -0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.anand_kumar.firstapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button answerYesButton , answerNoButton ;
+    private EditText userNameEditText ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        answerYesButton = (Button) findViewById(R.id.answer_yes_button);
+        answerNoButton = (Button) findViewById(R.id.answer_no_button);
+        userNameEditText = (EditText) findViewById(R.id.user_name_edit_text);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onYesClickButton(View view) {
+        String userName = String.valueOf(userNameEditText.getText());
+
+        String yourYesResponce = "That is Great " + userName;
+
+        Toast.makeText(this,yourYesResponce, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onNoClickButton(View view) {
+
+        String userName = String.valueOf(userNameEditText.getText());
+
+        String yourNoResponce = "To Bad " + userName;
+
+        Toast.makeText(this,yourNoResponce, Toast.LENGTH_LONG).show();
+
+    }
+}
